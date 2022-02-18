1afd4d5923eceff0071728d321969ae56f1b49be
Rittik kumar <kumarrittik060@gmail.com>
2022-02-18 21:57:17 +0530

    initial change

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ function Header() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            FireCommerce
+            EvaCommerce
           </Link>
           <button
             className="navbar-toggler"
